8699fea1ad199a4c4ef6f91791aac0b8463a5605
Tony Pham <tony.phamx04@gmail.com>
2021-08-20 13:42:08 -0400

    Miss Kobyashi: Add header and reference comments

--- piezo-tune-player.ino
+++ piezo-tune-player.ino
@@ -1,3 +1,13 @@
+/**
+ * Piezo Tune Player
+ *
+ * This sketch is a fan cover to the song 'Aozora no Rhapsody' by fhána from the anime 'Miss Kobayshi's Dragon Maid'.
+ * Using a piezo buzzer the main melody for the song is played.
+ *
+ * @author Tony Pham
+ */
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -7,3 +17,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+/**
+ * References
+ * 1. 'Aozora no Rhapsody' played with Synthesia uploaded by 'MyReminiscence': https://www.youtube.com/watch?v=sG9tKEV510s
+ * 2. 'Project 12 - Piezo Sounder Melody Player' from the 'Beginning Arduino' textbook written by Michael Roberts
+ */
