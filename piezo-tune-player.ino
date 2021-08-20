12d85359df55274958afa2b1f0330d22e4387b26
Tony Pham <tony.phamx04@gmail.com>
2021-08-20 15:14:20 -0400

    Miss Kobayashi: Add 'Chu, Chu Yeah' song intro

--- piezo-tune-player.ino
+++ piezo-tune-player.ino
@@ -7,19 +7,76 @@
  * @author Tony Pham
  */
 
+/* Note sounds frequencies */
+#define REST 0
+#define NOTE_G3 196
+#define NOTE_AS3 233
+#define NOTE_C5 523
+#define NOTE_D5 587
+#define NOTE_C4 262
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_A4 440
+
+ /* Note durations */
+#define WHOLE 1.0
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.125
+
+/* Global Variables */
+const int PIEZO_PIN = 15;
+int tuneLength;
+
+/* Tune Variables */
+const int missKobayashiOpTune[] = {
+  NOTE_C5, NOTE_D5, NOTE_C5,
+  NOTE_C4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_C5,
+  REST,
+  NOTE_D5, NOTE_C5,
+  NOTE_C4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_C5,
+  REST,
+  NOTE_D5, NOTE_D5, NOTE_C5,
+  NOTE_C4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_C5,
+  REST,
+  NOTE_D5, NOTE_C5,
+  NOTE_C4, NOTE_A4, NOTE_D5, NOTE_C5, NOTE_A4, NOTE_G4,
+  NOTE_G3, NOTE_G3, NOTE_G3, NOTE_G3,
+  NOTE_AS3, NOTE_AS3, NOTE_AS3, NOTE_AS3
+};
+const float missKobayashiOpDurations[] = {
+  QUARTER, QUARTER, QUARTER,
+  QUARTER, EIGHTH, EIGHTH, QUARTER, EIGHTH, EIGHTH,
+  QUARTER,
+  HALF, QUARTER,
+  QUARTER, EIGHTH, EIGHTH, QUARTER, EIGHTH, EIGHTH,
+  QUARTER,
+  QUARTER, QUARTER, QUARTER,
+  QUARTER, EIGHTH, EIGHTH, QUARTER, EIGHTH, EIGHTH,
+  QUARTER,
+  HALF, QUARTER,
+  QUARTER, HALF, EIGHTH, EIGHTH, EIGHTH, EIGHTH,
+  QUARTER, QUARTER, QUARTER, HALF,
+  QUARTER, QUARTER, QUARTER, HALF
+};
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(PIEZO_PIN, OUTPUT);
+  tuneLength = sizeof(missKobayashiOpTune) / sizeof(missKobayashiOpTune[0]);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (int i = 0; i < tuneLength; i++) {
+    tone(PIEZO_PIN, missKobayashiOpTune[i]);
+    delay(1000 * missKobayashiOpDurations[i]);
+    noTone(PIEZO_PIN);
+  }
+  delay(500);
 }
 
 /**
  * References
  * 1. 'Aozora no Rhapsody' played with Synthesia uploaded by 'MyReminiscence': https://www.youtube.com/watch?v=sG9tKEV510s
  * 2. 'Project 12 - Piezo Sounder Melody Player' from the 'Beginning Arduino' textbook written by Michael Roberts
+ * 3. Physics of Music - Notes: https://pages.mtu.edu/~suits/notefreqs.html
  */
