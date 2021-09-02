7cf86475be6355177a84f2d31676e29da58e3051
Tony Pham <tony.phamx04@gmail.com>
2021-09-02 17:27:09 -0400

    Seven Segment Display: Add function to update the 7-segment display with a number and function to test all numbers

--- piezo-tune-player.ino
+++ piezo-tune-player.ino
@@ -14,16 +14,39 @@
 #include "Playlist.h"
 #include "Error.h"
 
+/* Constants for 7-Segment Display */
+const byte DIGITAL_ZERO = {B00000011};
+const byte DIGITAL_ONE = {B10011111};
+const byte DIGITAL_TWO = {B00100101};
+const byte DIGITAL_THREE = {B00001101};
+const byte DIGITAL_FOUR = {B10011001};
+const byte DIGITAL_FIVE = {B01001001};
+const byte DIGITAL_SIX = {B01000001};
+const byte DIGITAL_SEVEN = {B00011111};
+const byte DIGITAL_EIGHT = {B00000001};
+const byte DIGITAL_NINE = {B00001001};
+
 /* Global Variables */
 const int PIEZO_PIN = 15;
+const int SER = 17;
+const int LATCH = 18;
+const int SRCLK = 19;
 
-void setup() {
+void setup()
+{
   pinMode(PIEZO_PIN, OUTPUT);
+  pinMode(SER, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(SRCLK, OUTPUT);
+
+  digitalWrite(SER, LOW);
+  digitalWrite(LATCH, LOW);
+  digitalWrite(SRCLK, LOW);
 }
 
-void loop() {
-  playSelectTune(AI_NO_SUPREME);
-  delay(500);
+void loop()
+{
+  testDigitalNumbers();
 }
 
 /**
@@ -70,6 +93,48 @@ void playSelectTune(int tune)
   }
 }
 
+/**
+ * Updates the 7-segment display with the specified number.
+ */
+void updateSevenSegmentDisplay(int numToDisplay)
+{
+  digitalWrite(LATCH, LOW);
+  switch(numToDisplay)
+  {
+    case 1:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_ONE);
+      break;
+    case 2:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_TWO);
+      break;
+    case 3:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_THREE);
+      break;
+    case 4:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_FOUR);
+      break;
+    case 5:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_FIVE);
+      break;
+    case 6:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_SIX);
+      break;
+    case 7:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_SEVEN);
+      break;
+    case 8:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_EIGHT);
+      break;
+    case 9:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_NINE);
+      break;
+    default:
+      shiftOut(SER, SRCLK, LSBFIRST, DIGITAL_ZERO);
+      break;
+  }
+  digitalWrite(LATCH, HIGH);
+}
+
 /* DEBUG/TEST FUNCTIONS */
 
 /**
@@ -78,6 +143,19 @@ void playSelectTune(int tune)
 void testPlaySelectTuneDefault()
 {
   playSelectTune(SUPER_MARIO_SUNSHINE_GAME_OVER);
+}
+
+/**
+ * This test function confirms that the digital constants are correct representations of their respective numbers by cycling through the numbers 0-9 and displaying them on the 7-segment display.
+ */
+void testDigitalNumbers()
+{
+  // There are 10 numbers to test from 0-9
+  for (int i = 0; i <= 9; i++)
+  {
+      updateSevenSegmentDisplay(i);
+      delay(1000);
+  }
 }
 
 /**
